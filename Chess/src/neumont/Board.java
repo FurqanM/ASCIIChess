af26a5c8b121f6413ff923ec420f677d888bfbe8
FurqanM <Furqan_Mohammed@yahoo.com>
2013-12-11 14:02:45 -0300

    Testing Merge

--- Chess/src/neumont/Board.java
+++ Chess/src/neumont/Board.java
@@ -2,6 +2,7 @@ package neumont;
 
 public class Board 
 {
-	// Board will be a 2D array of object Piece
+	// Board will be a 2D array -- of object Piece.
+	//testing merging and collision in GitHub
 	
 }
