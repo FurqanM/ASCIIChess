9119965b573d0607bc66f0f2c3b4739f91253230
FurqanM <Furqan_Mohammed@yahoo.com>
2013-12-12 09:32:45 -0700

    finished array set

--- Chess/src/neumont/Board.java
+++ Chess/src/neumont/Board.java
@@ -2,7 +2,10 @@ package neumont;
 
 public class Board 
 {
-	Piece boardSetup[][] = new Piece[8][8];
+	private int currentX = 0;
+	private int currentY = 0;
+	private Piece boardSetup[][] = new Piece[8][8];
+	
 	
 	public Board()
 	{
@@ -14,8 +17,32 @@ public class Board
 		boardSetup[0][5] = new Piece(PieceType.WBISHOP);
 		boardSetup[0][6] = new Piece(PieceType.WKNIGHT);
 		boardSetup[0][7] = new Piece(PieceType.WROOK);
+		boardSetup[1][0] = new Piece(PieceType.WPAWN);
+		boardSetup[1][1] = new Piece(PieceType.WPAWN);
+		boardSetup[1][2] = new Piece(PieceType.WPAWN);
+		boardSetup[1][3] = new Piece(PieceType.WPAWN);
+		boardSetup[1][4] = new Piece(PieceType.WPAWN);
+		boardSetup[1][5] = new Piece(PieceType.WPAWN);
+		boardSetup[1][6] = new Piece(PieceType.WPAWN);
+		boardSetup[1][7] = new Piece(PieceType.WPAWN);
 		
 		
+		boardSetup[7][0] = new Piece(PieceType.BROOK);
+		boardSetup[7][1] = new Piece(PieceType.BKNIGHT);
+		boardSetup[7][2] = new Piece(PieceType.BBISHOP);
+		boardSetup[7][3] = new Piece(PieceType.BQUEEN);
+		boardSetup[7][4] = new Piece(PieceType.BKING);
+		boardSetup[7][5] = new Piece(PieceType.BBISHOP);
+		boardSetup[7][6] = new Piece(PieceType.BKNIGHT);
+		boardSetup[7][7] = new Piece(PieceType.BROOK);
+		boardSetup[6][0] = new Piece(PieceType.BPAWN);
+		boardSetup[6][1] = new Piece(PieceType.BPAWN);
+		boardSetup[6][2] = new Piece(PieceType.BPAWN);
+		boardSetup[6][3] = new Piece(PieceType.BPAWN);
+		boardSetup[6][4] = new Piece(PieceType.BPAWN);
+		boardSetup[6][5] = new Piece(PieceType.BPAWN);
+		boardSetup[6][6] = new Piece(PieceType.BPAWN);
+		boardSetup[6][7] = new Piece(PieceType.BPAWN);
 	}
 	
 	
