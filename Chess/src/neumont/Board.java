34959f048c528ed96b5217ec767fcec13fe3c3aa
Siryu <coreymassey@gmail.com>
2013-12-11 21:51:03 -0300

    Some initial setup to get going.

--- Chess/src/neumont/Board.java
+++ Chess/src/neumont/Board.java
@@ -2,11 +2,21 @@ package neumont;
 
 public class Board 
 {
-	// 2D array to show where each piece is
+	Piece boardSetup[][] = new Piece[8][8];
 	
-	
-	
-	// constructor to setup initial board placements
+	public Board()
+	{
+		boardSetup[0][0] = new Piece(PieceType.WROOK);
+		boardSetup[0][1] = new Piece(PieceType.WKNIGHT);
+		boardSetup[0][2] = new Piece(PieceType.WBISHOP);
+		boardSetup[0][3] = new Piece(PieceType.WQUEEN);
+		boardSetup[0][4] = new Piece(PieceType.WKING);
+		boardSetup[0][5] = new Piece(PieceType.WBISHOP);
+		boardSetup[0][6] = new Piece(PieceType.WKNIGHT);
+		boardSetup[0][7] = new Piece(PieceType.WROOK);
+		
+		
+	}
 	
 	
 	// method to draw board looping through array
--- Chess/src/neumont/Piece.java
+++ Chess/src/neumont/Piece.java
@@ -1,83 +1,31 @@
 package neumont;
 
-public enum Piece
+public class Piece
 {
+	String picture;
+	PieceType type;
 	
-	BPAWN, BBISHOP, BROOK, BKNIGHT, BQUEEN, BKING,
-	WPAWN, WBISHOP, WROOK, WKNIGHT, WQUEEN, WKING;
-	
-	// String piece type -- pawn, knight, rook, bishop, king, queen
-	// String : ascii chararcter for the piece
-
-	
-	// create move list in a method
-	// x,y coord : Move type // bottom left 0,0
-	// also pawn initial movement can be 2 forward
-	public void Move()
+	public Piece(PieceType type)
 	{
-		//Moves pieces
-		if(this.equals(BPAWN))
-		{
-			//move y - 1
-			//or ((x - 1) && (y - 1)) || ((x + 1) && (y - 1))
-			//move one space down
-			//if has not moved yet then move two spaces y = 7
-		}
-		
-		else if(this.equals(WPAWN))
-		{
-			//move y + 1
-			//or ((x + 1) && (y + 1)) || ((x - 1) && (y + 1))
-			//move one space up
-			//if has not moved yet then move two spaces y = 2
-		}
-		
-		else if(this.equals(BBISHOP) || this.equals(WBISHOP))
-		{
-			//move (x - 1 && y - 1) || (x + 1 && y + 1)
-			//(x - 1 && y + 1) || (x + 1 && y - 1)
-		}
-		
-		else if(this.equals(BROOK) || this.equals(WROOK))
-		{
-			//move (min)X to (max)X || (min)Y to (max)Y
-		}
-		
-		else if(this.equals(BKNIGHT) || this.equals(WKNIGHT))
-		{
-			
-			/*
-			//vertical
-			(y + 2) && (x + 1)
-			(y + 2) && (x - 1)
-			
-			(y - 2) && (x + 1)
-			(y - 2) && (x - 1)
-			
-			
-			//horizontal
-			(x + 2) && (y + 1)
-			(x + 2) && (y - 1)
-			
-			(x - 2) && (y + 1)
-			(x - 2) && (y - 1)
-			*/
-		}
-		
-		else if(this.equals(BQUEEN) || this.equals(WQUEEN))
-		{
-			//move Rook + Bishop
-		}
-		
-		else if(this.equals(BKING) || this.equals(WKING))
-		{
-			//move (y - 1) || (y + 1)
-			//(x + 1) || (x - 1)
-			//or ((x - 1) && (y - 1)) || (x + 1) && (y - 1)
-			//or ((x + 1) && (y + 1)) || ( x - 1) && ( y + 1))
-		}
+		this.type = type;
 	}
 	
 	
+	
+	public String getPicture()
+	{
+		return picture;
+	}
+	public void setPicture(String picture)
+	{
+		this.picture = picture;
+	}
+	public PieceType getType()
+	{
+		return type;
+	}
+	public void setType(PieceType type)
+	{
+		this.type = type;
+	}
 }
-
